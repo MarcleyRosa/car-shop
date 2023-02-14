94166bf14db2e0f0c00784f349e8d25002c040dd
Marcley <marcleyrosa@gmail.com>
2023-02-14 20:03:00 -0300

    feat: exception 04

--- src/Middleware/MiddlewareError.ts
+++ src/Middleware/MiddlewareError.ts
@@ -3,6 +3,7 @@ import HttpException from '../Exceptions/Exception';
 
 const middlewareError: ErrorRequestHandler = (err, _req, res, _next) => {
   console.log('err', err);
+  if (err.kind === 'ObjectId') return res.status(422).json({ message: 'Invalid mongo id' });
   const { status, message } = err as HttpException;
   res.status(status || 500).json({ message });
 };
